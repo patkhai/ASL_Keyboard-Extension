9cf364bb80ac083e7afb893ae1f0b6d7809c6f22
Kam Khai <patrick.phyo@gmail.com>
2019-02-16 17:49:50 -0800

    Initial Commit

---
+++ ASL/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ASL
+//
+//  Created by Pat Khai on 2/16/19.
+//  Copyright © 2019 Pat Khai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
